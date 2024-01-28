997cc4d76d42ab49405deb111be8017e5631cf34
Artem Karelin <temi41995@gmail.com>
2024-01-28 23:40:23 +0100

    feat: copy task completed

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { cp } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const folder2Copy = path.join(__dirname, ".", "files");
+const copyDest = path.join(__dirname, ".", "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  cp(folder2Copy, copyDest, {
+    recursive: true,
+    force: false,
+    errorOnExist: true,
+  }).catch(() => console.log(new Error("FS operation failed")));
 };
 
 await copy();
