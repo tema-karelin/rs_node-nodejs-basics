3a70e104c9026035bd69880e0266941ee47b5323
Artem Karelin <temi41995@gmail.com>
2024-01-28 19:51:54 +0100

    feat: create task completed

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile, access } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const fileName = 'fresh.txt';
+const filePath = path.join(__dirname, '.', 'files', fileName); 
+const data = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  await access(filePath)
+  .then(() => {
+    const err = new Error('FS operation failed');
+    console.log(err);
+  })
+  .catch(() => {
+    writeFile(filePath, data, 'utf-8')
+    .catch((err) => console.log("Creating file error: ", err));
+  });    
 };
 
 await create();
