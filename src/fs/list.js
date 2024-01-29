b51f0d139ff428f8c4307f131c7503bcf170964b
Artem Karelin <temi41995@gmail.com>
2024-01-29 01:03:23 +0100

    feat: list task completed

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import path from "node:path";
+import { fileURLToPath } from "url";
+import { readdir } from "node:fs/promises";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const folder = 'files';
+const dirPath = path.join(__dirname, ".", folder);
+const errMsg = "FS operation failed";
+
 const list = async () => {
-    // Write your code here 
+    readdir(dirPath)
+    .then(res => {
+        res.forEach((file) => console.log(' - ', file));
+    })
+    .catch(() => (console.log(new Error(errMsg))));
 };
 
 await list();
