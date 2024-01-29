5528d69dcf19cccbc6af6820f69f9429b489189e
Artem Karelin <temi41995@gmail.com>
2024-01-29 15:42:49 +0100

    feat: modules task completed

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,22 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import { fileURLToPath } from "url";
+import path from "path";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import * as a from "./files/a.json" with {type: 'json'};
+import * as b from "./files/b.json" with {type: 'json'};
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = a;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = b;
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +39,4 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
-
+export {unknownObject, myServer};
