c84255d86735233694fc6b42ceb29928e26cb1e4
Artem Karelin <temi41995@gmail.com>
2024-01-29 20:34:42 +0100

    feat: compress & decompress task completed

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
-const decompress = async () => {
-    // Write your code here 
+// decompress.js - implement function that decompresses archive.gz back to the fileToCompress.txt with same content as before compression using zlib and Streams API
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream";
+import { promisify } from "util";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, ".", "files", "fileToCompress.txt");
+const archivePath = path.join(__dirname, ".", "files", "archive.gz");
+
+const decompress = async (source, destination) => {
+    const sourseStream = createReadStream(source);
+    const destStream = createWriteStream(destination);
+    
+    const pipe = promisify(pipeline);
+
+    await pipe(sourseStream, createGunzip(), destStream);
 };
 
-await decompress();
+await decompress(archivePath, filePath);
