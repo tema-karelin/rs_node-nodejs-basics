5f08519eb3a271b6fb100085bb4ad980e2ed6906
Artem Karelin <temi41995@gmail.com>
2024-01-29 23:28:56 +0100

    feat: worker threads task is completed

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,19 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+// worker.js - extend given function to work with data received from main thread and implement function which sends result of the computation to the main thread
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+import { workerData, parentPort } from "worker_threads";
+
+// n should be received from main thread
+const { n, id } = workerData;
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = async () => {
+  const result = nthFibonacci(n);
+  parentPort.postMessage({
+    id,
+    result,
+  });
 };
 
 sendResult();
