dd0b09b8f2486c8116c03ca6633c94d898772519
Artem Karelin <temi41995@gmail.com>
2024-01-29 01:10:09 +0100

    feat: read task completed

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import path from "node:path";
+import { fileURLToPath } from "url";
+import { readFile } from "node:fs/promises";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, ".", "files", "fileToRead.txt");
+const errMsg = "FS operation failed";
+
 const read = async () => {
-    // Write your code here 
+  readFile(filePath, "utf-8")
+    .then((res) => {
+      console.log("\n" + res);
+    })
+    .catch(() => console.log(new Error(errMsg)));
 };
 
 await read();
